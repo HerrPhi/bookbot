2c99ca88bff3789bedd0a4f88940792d66bab838
HerrPhi <fingatta@gmail.com>
2025-09-02 12:30:30 +0200

    added functionality to print a book report

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from stats import get_num_words
 from stats import count_characters
+from stats import sort_dictionary_into_list
 
 #function get_book_text:
 def get_book_text(filepath):
@@ -10,10 +11,24 @@ def get_book_text(filepath):
 
 def main():
     #print(get_book_text("books/frankenstein.txt"))
+
     number_of_words = get_num_words(get_book_text("books/frankenstein.txt"))
-    print(f"{number_of_words} words found in the document")
+    #print(f"{number_of_words} words found in the document")
 
     dictionary_of_characters = count_characters(get_book_text("books/frankenstein.txt"))
-    print(dictionary_of_characters)
+    #print(dictionary_of_characters)
+
+    sorted_list_of_dictionarys = sort_dictionary_into_list(dictionary_of_characters)
+
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
+    print(f"Found {number_of_words} total words")
+    print("--------- Character Count -------")
+    for dictionary in sorted_list_of_dictionarys:
+        if dictionary["char"].isalpha():
+            print(f"{dictionary["char"]}: {dictionary["num"]}")
+    print("============= END ===============")
+
 
 main()
--- stats.py
+++ stats.py
@@ -18,3 +18,22 @@ def count_characters(text):
             else:
                 characters[letter] = 1  #adds the character to the dictionary
     return characters   #returns the dictionary with the characters and their counts
+
+#returns sorted list of dictionarys
+def sort_dictionary_into_list(dictionary):
+    sorted_list_of_dictionarys = list()
+
+    for char in dictionary:
+        new_dict = dict()
+        num = dictionary[char]
+        new_dict = {"char" : char, "num" : dictionary[char]}
+        sorted_list_of_dictionarys.append(new_dict)
+
+    
+    sorted_list_of_dictionarys.sort(reverse=True,key=sort_on)
+    #print(sorted_list_of_dictionarys)
+
+    return sorted_list_of_dictionarys
+
+def sort_on(dict):
+    return dict["num"]
